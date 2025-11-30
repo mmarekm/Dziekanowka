a7716f5a7e515744807164807f92afff27513523
Marek <marekwpracymalicki@gmail.com>
2025-11-30 17:46:43 +0100

    poczatki u studziennego

--- Dziekanowka/Gracz.cs
+++ Dziekanowka/Gracz.cs
@@ -6,6 +6,7 @@ namespace Dziekanowka
         public string Nazwa { get; set;  } = nazwa;
         public int Doswiadczenie { get; set; } = 0;
         public int Monety { get; set; } = 0;
+        public int Butelki { get; set; } = 2;
         public bool MoznaRozszerzycGospodarstwo { get; set; } = false;
         public List<Budynek> Budynki = [new("studnia", 2)];
     }
