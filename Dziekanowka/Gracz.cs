439427b69058e5c17a014c475327c8980381964c
Marek <marekwpracymalicki@gmail.com>
2025-12-01 09:49:31 +0100

    prace przy studni

--- Dziekanowka/Gracz.cs
+++ Dziekanowka/Gracz.cs
@@ -6,8 +6,11 @@ namespace Dziekanowka
         public string Nazwa { get; set;  } = nazwa;
         public int Doswiadczenie { get; set; } = 0;
         public int Monety { get; set; } = 0;
-        public int Butelki { get; set; } = 2;
+        public List<PrzedmiotGracza> Przedmioty { get; set; } = PrzedmiotGracza.StartowePrzedmioty();
         public bool MoznaRozszerzycGospodarstwo { get; set; } = false;
-        public List<Budynek> Budynki = [new("studnia", 2)];
+        public List<BudynekGracza> Budynki { get; set; } = [new("studnia", 2)];
+        public int PoziomBudynku(string nazwaBudynku) => Budynki.First(b => b.Nazwa == nazwaBudynku).Poziom;
+        public int KosztUlepszeniaBudynku(string nazwaBudynku) => nazwaBudynku switch { "studnia" => PoziomBudynku("studnia") * 1000, _ => 0 };
+        public int IloscPrzedmiotow(string nazwaPrzedmiotu) => Przedmioty.First(p => p.Nazwa == nazwaPrzedmiotu).Ilosc;
     }
 }
--- Dziekanowka/Gracza/BudynekGracza.cs
+++ Dziekanowka/Gracza/BudynekGracza.cs
@@ -1,10 +1,9 @@
 ﻿namespace Dziekanowka.Gracza
 {
-    public class Budynek(string nazwa, int limitPoziomow)
+    public class BudynekGracza(string nazwa, int limitPoziomow)
     {
         public string Nazwa { get; set; } = nazwa;
         public int Poziom { get; set; } = 1;
         public int LimitPoziomow { get; set; } = limitPoziomow;
-        public int KosztUlepszenia() => Nazwa switch { "studnia" => Poziom * 1000, _ => 0 };
     }
 }
