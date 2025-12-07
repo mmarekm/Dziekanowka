acad2fcb3de5ca9ff93c23dad5ea6ab5d7588fe8
Marek <marekwpracymalicki@gmail.com>
2025-12-07 17:33:39 +0100

    warzywa na polach

--- Dziekanowka/Gracza/PoleUprawne.cs
+++ Dziekanowka/Gracza/PoleUprawne.cs
@@ -9,10 +9,13 @@
         public string Warzywo { get; set; } = warzywo;
         public static List<PoleUprawne> StartowePola()
         {
-            List<string> w = [ "kukurydza", "groch", "jarmuż", "pasternak", "pietruszka", "burak", "brukselka", "sorgo", "rzepaPastewna", "koniczyna", "sałata", "marchew", "ziemniak",
-                       "cebula", "szczypiorek", "pomidor", "papryka", "ogórek", "czosnek", "rzodkiewka" ];
-            int i = 0;
-            return (from id in new[] { 1, 2 } from x in Enumerable.Range(1, 5) from y in Enumerable.Range(1, 4) select new PoleUprawne(id, x, y, id == 1 ? w[i++] : "")).ToList();
+            List<string> w1 = [ "kukurydza", "groch", "jarmuż", "pasternak", "pietruszka", "burak", "brukselka", "sorgo", "rzepaPastewna", "koniczyna", "sałata", "marchew", "ziemniak",
+               "cebula", "szczypiorek", "pomidor", "papryka", "ogórek", "czosnek", "rzodkiewka" ];
+            List<string> w2 = [ "szpinak", "rukola", "batat", "szczaw", "seler", "boćwina", "por", "fasola", "bób", "ciecierzyca", "bakłażan", "cukinia", "dynia",
+               "brokuł", "kalafior", "kapustaWłoska", "kapustaPekińska", "szparagi", "selerNaciowy", "soczewica" ];
+            int i1 = 0;
+            int i2 = 0;
+            return (from id in new[] { 1, 2 } from x in Enumerable.Range(1, 5) from y in Enumerable.Range(1, 4) select new PoleUprawne(id, x, y, id == 1 ? w1[i1++] : w2[i2++])).ToList();
         }
     }
 }
