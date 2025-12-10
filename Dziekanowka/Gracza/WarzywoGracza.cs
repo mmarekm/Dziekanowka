88d50531b31ce84e79769d7d2212267da9f75655
marek <marekwpracymalicki@gmail.com>
2025-12-10 10:16:21 +0100

    warzywo plus

--- Dziekanowka/Gracza/WarzywoGracza.cs
+++ Dziekanowka/Gracza/WarzywoGracza.cs
@@ -6,7 +6,7 @@
         public int Ilosc { get; set; } = 0;
         public static List<WarzywoGracza> StartoweWarzywa() => [new("kukurydza"), new("groch"), new("jarmuż"), new("pasternak"), new("pietruszka"), new("burak"), new("brukselka"), 
             new("sorgo"), new("rzepaPastewna"), new("koniczyna"), new("sałata"), new("marchew"), new("ziemniak"), new("cebula"), new("szczypiorek"), new("pomidor"), new("papryka"),
-            new("ogórek"), new("czosnek"), new("rzodkiewka"), new("szpinak"), new("rukola"), new("batat"), new("szczaw"), new("seler"), new("boćwina"), new("por"), new("fasola"),
+            new("ogórek"), new("czosnek"), new("rzodkiewka"), new("szpinak"), new("rukola"), new("batat"), new("szczaw"), new("seler"), new("botwina"), new("por"), new("fasola"),
             new("bób"), new("ciecierzyca"), new("bakłażan"), new("cukinia"), new("dynia"), new("brokuł"), new("kalafior"), new("kapustaWłoska"), new("kapustaPekińska"), new("szparagi"),
             new("selerNaciowy"), new("soczewica")];
     }
