d39a4562b6ca0f07308237630e70aa834aa6e5d4
Marek <marekwpracymalicki@gmail.com>
2025-12-10 17:21:53 +0100

    poczatek listy owocow

--- Dziekanowka/Gracz.cs
+++ Dziekanowka/Gracz.cs
@@ -10,6 +10,7 @@ namespace Dziekanowka
         public List<BudynekGracza> Budynki { get; set; } = BudynekGracza.StartoweBudynki();
         public List<PoleUprawne> PolaUprawne { get; set; } = PoleUprawne.StartowePola();
         public List<WarzywoGracza> Warzywa { get; set; } = WarzywoGracza.StartoweWarzywa();
+        public List<OwocGracza> Owoce { get; set; } = OwocGracza.StartoweOwoce();
         public Paliwo Paliwo { get; set; } = new Paliwo();
         public int PoziomBudynku(string nazwaBudynku) => Budynki.First(b => b.Nazwa == nazwaBudynku).Poziom;
         public void UlepszBudynek(string nazwaBudynku) => Budynki.First(b => b.Nazwa == nazwaBudynku).Poziom++;
---
+++ Dziekanowka/Gracza/OwocGracza.cs
@@ -0,0 +1,10 @@
+﻿namespace Dziekanowka.Gracza
+{
+    public class OwocGracza(string nazwa)
+    {
+        public string Nazwa { get; set; } = nazwa;
+        public int Ilosc { get; set; } = 0;
+        public static List<OwocGracza> StartoweOwoce() => [new("brzoskwinia"), new("nektarynka"), new("morela"), new("śliwka"), new("wiśnia"), new("czereśnia"), new("jabłko"), new("gruszka"),
+		new("truskawka"), new("malina")];
+    }
+}
