db4ba538826abb99e12c13c4e2faaee979f065c4
Marek <marekwpracymalicki@gmail.com>
2025-12-21 09:09:17 +0100

    koszenie done S

--- Dziekanowka/Gracza/PrzedmiotGracza.cs
+++ Dziekanowka/Gracza/PrzedmiotGracza.cs
@@ -4,6 +4,6 @@
     {
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = ilosc;
-        public static List<PrzedmiotGracza> StartowePrzedmioty() => [ new("pustaButelka", 2), new("pelnaButelka", 0), new("siano", 0) ];
+        public static List<PrzedmiotGracza> StartowePrzedmioty() => [ new("pustaButelka", 2), new("pelnaButelka", 0), new("siano", 2) ];
     }
 }
