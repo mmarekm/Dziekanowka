0e72ca84ab57b193ce0ef54d05c225e46dd8d0cc
Marek <marekwpracymalicki@gmail.com>
2025-12-23 11:53:06 +0100

    start static Gra

---
+++ Dziekanowka/Mechanizm/Gra.cs
@@ -0,0 +1,7 @@
+﻿namespace Dziekanowka.Mechanizm
+{
+    public static class Gra
+    {
+        public static bool JestZwierze(Gracz g, string zwierz) => g.Zwierzeta.Any(z => z.Nazwa == zwierz && z.Ilosc > 0);
+    }
+}
