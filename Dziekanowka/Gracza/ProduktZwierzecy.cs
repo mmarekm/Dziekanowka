b01cef1c65acd321bf630195c15026ccaa37306f
Marek <marekwpracymalicki@gmail.com>
2026-01-11 09:58:00 +0100

    sklep z drobiem done

--- Dziekanowka/Gracza/ProduktZwierzecy.cs
+++ Dziekanowka/Gracza/ProduktZwierzecy.cs
@@ -5,11 +5,10 @@
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = ilosc;
         public static List<ProduktZwierzecy> StartoweProdukty() => [
-            new("jajoKurze"), new("kuraJedzenie"), new("gesPioro"), new("jajoGesie"), new("gesJedzenie"), new("gesSmalec"), new("jajoIndycze"), new("indykJedzenie"),
-            new("kaczkaPioro"), new("jajoKacze"), new("kaczkaJedzenie"), new("kaczkaSmalec"), new("futroKrolik"), new("krolikJedzenie"), new("swiniaSzynka"), new("swiniaSchab"),
-            new("swiniaBoczek"), new("swiniaSmalec"), new("mlekoOwcze"), new("owcaJedzenie"), new("owcaSkora"), new("owcaWelna"), new("alpakaWelna"), new("mlekoKozie"), new("kozaJedzenie"),
-            new("kozaSkora"), new("mlekoKrowie"), new("krowaRostbef"), new("krowaAntrykot"), new("krowaLopatka"), new("krowaPoledwica"), new("krowaSkora")
+            new("kuraJajo"), new("kuraJedzenie"), new("gesPioro"), new("gesJajo"), new("gesJedzenie"), new("gesSmalec"), new("indykJajo"), new("indykJedzenie"),
+            new("kaczkaPioro"), new("kaczkaJajo"), new("kaczkaJedzenie"), new("kaczkaSmalec"), new("futroKrolik"), new("krolikJedzenie"), new("swiniaSzynka"), new("swiniaSchab"),
+            new("swiniaBoczek"), new("swiniaSmalec"), new("owcaMleko"), new("owcaJedzenie"), new("owcaSkora"), new("owcaWelna"), new("alpakaWelna"), new("kozaMleko"), new("kozaJedzenie"),
+            new("kozaSkora"), new("krowaMleko"), new("krowaRostbef"), new("krowaAntrykot"), new("krowaLopatka"), new("krowaPoledwica"), new("krowaSkora")
         ];
     }
 }
-// przy zakupie nakarmione -> 0 wiec kura kupowana z jednym jajem ges tez kaczka tez, indyk tez, krowa z banka mleka, koza tez, krolik i swinia NIE
