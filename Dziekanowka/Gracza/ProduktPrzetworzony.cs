a883f348b4c17d74188e0eacd7953864e9642abe
Marek <marekwpracymalicki@gmail.com>
2026-01-14 13:10:30 +0100

    start zarna

--- Dziekanowka/Gracza/ProduktPrzetworzony.cs
+++ Dziekanowka/Gracza/ProduktPrzetworzony.cs
@@ -5,7 +5,8 @@
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = ilosc;
         public static List<ProduktPrzetworzony> StartoweProdukty() => [
-        new("smietana"), new("maslo"), new("majonez"), new("krowaSer"), new("kozaSer"), new("owcaSer"), new("twarog")
+        new("smietana"), new("maslo"), new("majonez"), new("krowaSer"), new("kozaSer"), new("owcaSer"), new("twarog"), new("makaron"),
+        new("zytoMaka"), new("jeczmienMaka"), new("pszenicaMaka"), new("ryzMaka"), new("owiesMaka"), new("grykaMaka"), new("orkiszMaka")
         ];
     }
 }
