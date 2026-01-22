e9f538a5e68cd6c49d6a5aa7d0fa549c586545f2
Marek <marekwpracymalicki@gmail.com>
2026-01-22 09:45:01 +0100

    zakwas start

--- Dziekanowka/Gracza/ZbozeGracza.cs
+++ Dziekanowka/Gracza/ZbozeGracza.cs
@@ -4,6 +4,7 @@
     {
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = 0;
+        public int PoziomZakwasu { get; set; } = 0;
         public static List<ZbozeGracza> StartoweZboza() => [new("zyto"), new("jeczmien"), new("pszenica"), new("ryz"), new("owies"), new("gryka"), new("orkisz")];
     }
 }
--- Dziekanowka/Gracza/ZywnoscPozostale.cs
+++ Dziekanowka/Gracza/ZywnoscPozostale.cs
@@ -4,6 +4,9 @@
     {
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = ilosc;
-        public static List<ZywnoscPozostale> StartowaZywnosc() => [ new("olej", 2), new("sol", 2), new("ziarnaKawy"), new("kawa") ];
+        public static List<ZywnoscPozostale> StartowaZywnosc() => [
+            new("olej", 2), new("sol", 2), new("proszekDoPieczenia", 1), new("kminek", 1), new("czarnyPieprz", 1), new("oregano"), new("cynamon"),
+            new("ziarnaKawy"), new("kawa")
+            ];
     }
 }
