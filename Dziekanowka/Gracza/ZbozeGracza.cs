458b01d31c667aaca454dd87557d51f0e26a5189
Marek <marekwpracymalicki@gmail.com>
2026-01-23 12:29:39 +0100

    zakwas done

--- Dziekanowka/Gracza/ZbozeGracza.cs
+++ Dziekanowka/Gracza/ZbozeGracza.cs
@@ -5,6 +5,9 @@
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = 0;
         public int PoziomZakwasu { get; set; } = 0;
+        public int OstatniDzienZakwaszenia { get; set; } = 0;
+        public int OstatniMiesiacZakwaszenia { get; set; } = 0;
+        public bool MoznaZakwasicDzisiaj => OstatniDzienZakwaszenia != DateTime.Now.Day || OstatniMiesiacZakwaszenia != DateTime.Now.Month;
         public static List<ZbozeGracza> StartoweZboza() => [new("zyto"), new("jeczmien"), new("pszenica"), new("ryz"), new("owies"), new("gryka"), new("orkisz")];
     }
 }
--- Dziekanowka/Gracza/ZywnoscPozostale.cs
+++ Dziekanowka/Gracza/ZywnoscPozostale.cs
@@ -5,7 +5,7 @@
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = ilosc;
         public static List<ZywnoscPozostale> StartowaZywnosc() => [
-            new("olej", 2), new("sol", 2), new("proszekDoPieczenia", 1), new("kminek", 1), new("czarnyPieprz", 1), new("oregano"), new("cynamon"),
+            new("olej", 2), new("sol", 2), new("proszekDoPieczenia", 1), new("czarnyPieprz", 1), new("oregano"), new("cynamon"),
             new("ziarnaKawy"), new("kawa")
             ];
     }
