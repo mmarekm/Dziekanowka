c9f1fc767455fd2ecab2f2c5d83ca84f9ac59781
Marek <marekwpracymalicki@gmail.com>
2026-01-27 13:59:36 +0100

    troche wedlin

--- Dziekanowka/Gracza/ProduktPrzetworzony.cs
+++ Dziekanowka/Gracza/ProduktPrzetworzony.cs
@@ -5,11 +5,11 @@
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = ilosc;
         public static List<ProduktPrzetworzony> StartoweProdukty() => [
-        new("smietana"), new("maslo"), new("majonez"), new("krowaSer"), new("kozaSer"), new("owcaSer"), new("twarog"), new("makaron"), new("kielbasa"),
+        new("smietana"), new("maslo"), new("majonez"), new("krowaSer"), new("kozaSer"), new("owcaSer"), new("twarog"), new("makaron"),
         new("zytoMaka"), new("jeczmienMaka"), new("pszenicaMaka"), new("ryzMaka"), new("owiesMaka"), new("grykaMaka"), new("orkiszMaka"),
         new("zytoPlatki"), new("jeczmienPlatki"), new("pszenicaPlatki"), new("ryzPlatki"), new("owiesPlatki"), new("grykaPlatki"), new("orkiszPlatki"), new("kukurydzaPlatki"),
         new("wino"), new("ogorekKiszony"), new("kapustaKiszona"), new("burakKiszony"), new("mieloneDrob"), new("mieloneDzikie"), new("mieloneSwinia"), new("mieloneKrowa"),
-        new("lopatkaWedlina"), new("poledwicaWedlina"), new("boczekWedlina"), new("szynkaWedlina"), new("schabWedlina"), new("kurczakWedlina"), new("indykWedlina"),
+        new("lopatkaWedlina"), new("poledwicaWedlina"), new("boczekWedlina"), new("szynkaWedlina"), new("schabWedlina"), new("kurczakWedlina"), new("indykWedlina"), new("salamiWedlina"), new("kielbasaWedlina"),
         new("krolikPasztet"), new("drobPasztet"), new("swiniaPasztet"),
         new("marchewSok"), new("burakSok"), new("pomidorSok"), new("jabłkoSok"), new("pomarańczaSok"), new("grejpfrutSok"), new("cytrynaSok"), new("ananasSok"),
         new("brzoskwiniaSok"), new("morelaSok"), new("wiśniaSok"), new("truskawkaSok"), new("malinaSok"), new("borówkaSok"), new("porzeczkaCzarnaSok"),
