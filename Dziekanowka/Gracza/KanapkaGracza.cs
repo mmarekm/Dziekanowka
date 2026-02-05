0d82dbf0a69a9ce2022937163b9b2560ee43db99
marek <marekwpracymalicki@gmail.com>
2026-02-05 16:00:04 +0100

    ruch kanapka

--- Dziekanowka/Gracza/KanapkaGracza.cs
+++ Dziekanowka/Gracza/KanapkaGracza.cs
@@ -1,11 +1,11 @@
 ﻿namespace Dziekanowka.Gracza
 {
-    public class KanapkaGracza(string pieczywo, string? tluszcz = null, string? wedlina = null, string? ser = null, HashSet<string>? dodatki = null)
+    public class KanapkaGracza(string pieczywo, string? tluszcz = null, string? ser = null, string? wedlina = null, HashSet<string>? dodatki = null)
     {
         public string Pieczywo { get; set; } = pieczywo;
         public string? Tluszcz { get; set; } = tluszcz;
-        public string? Wedlina { get; set; } = wedlina;
         public string? Ser { get; set; } = ser;
+        public string? Wedlina { get; set; } = wedlina;
         public HashSet<string> Dodatki { get; set; } = dodatki ?? [];
     }
 }
