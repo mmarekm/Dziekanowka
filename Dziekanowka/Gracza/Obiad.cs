229e73bd7df5968d852738b32d9a0708dc68830d
marek <marekwpracymalicki@gmail.com>
2026-02-10 20:45:18 +0100

    pare foto

--- Dziekanowka/Gracza/Obiad.cs
+++ Dziekanowka/Gracza/Obiad.cs
@@ -4,6 +4,7 @@
     {
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = 0;
-        public static List<Obiad> StartoweObiady() => [new("jajecznica"), new("jajkoSadzone"), new("omlet"), new("shakshuka"), new("kotletSchabowy")];
+        public static List<Obiad> StartoweObiady() => [new("jajecznica"), new("jajkoSadzone"), new("omlet"), new("shakshuka"), new("kotletSchabowy"), new("kotletMielony"), new("stek"),
+        new("piersZKurczaka"), new("bitki"), new("indykSmazony"), new("krolikSmazony"), new("pstragSmazony"), new("lososSmazony"), new("halibutSmazony"), new("okonSmazony"), new("sledzSmazony")];
     }
 }
