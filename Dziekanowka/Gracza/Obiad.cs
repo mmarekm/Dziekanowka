60fc86d435f0718cea1b62ea8d0246dd0c395958
Marek <marekwpracymalicki@gmail.com>
2026-02-12 12:29:49 +0100

    patelnia rusza powoli

--- Dziekanowka/Gracza/Obiad.cs
+++ Dziekanowka/Gracza/Obiad.cs
@@ -5,9 +5,9 @@
         public string Nazwa { get; set; } = nazwa;
         public int Ilosc { get; set; } = 0;
         public static List<Obiad> DaniaZPatelni = [new("jajecznica"), new("jajkoSadzone"), new("omlet"), new("shakshuka"), new("kotletSchabowy"), new("kotletMielony"), new("stek"),
-        new("piersZKurczaka"), new("bitki"), new("karkowka"), new("indykSmazony"), new("krolikSmazony"), new("golabki"), new("pstragSmazony"), new("lososSmazony"), new("halibutSmazony"), new("okonSmazony"), new("sledzSmazony"),
+        new("piersZKurczaka"), new("bitki"), new("indykSmazony"), new("krolikSmazony"), new("golabki"), new("pstragSmazony"), new("lososSmazony"), new("halibutSmazony"), new("okonSmazony"), new("sledzSmazony"),
         new("plackiZiemniaczane"), new("frytki"), new("pierogiRuskie"), new("pierogiZMiesem"), new("pierogiZKapustaIGrzybami"), new("pierogiZOwocami"), new("kopytka"), new("knedle"), new("nalesniki"), new("racuchy")];
-        public static List<Obiad> DaniaZGarnka = [];
+        public static List<Obiad> DaniaZGarnka = [new("karkowka")];
         public static List<Obiad> StartoweObiady() => DaniaZPatelni.Concat(DaniaZGarnka).Select(d => new Obiad(d.Nazwa)).ToList();
     }
 }
