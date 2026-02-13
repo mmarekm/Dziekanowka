c13edb7312432ff9894280419bf0f35c9138c9ee
Marek <marekwpracymalicki@gmail.com>
2026-02-13 17:05:49 +0100

    szukanie zup

--- Dziekanowka/Gracza/Obiad.cs
+++ Dziekanowka/Gracza/Obiad.cs
@@ -7,7 +7,8 @@
         public static List<Obiad> DaniaZPatelni = [new("jajecznica"), new("jajkoSadzone"), new("omlet"), new("shakshuka"), new("kotletSchabowy"), new("kotletMielony"), new("stek"),
         new("piersZKurczaka"), new("bitki"), new("indykSmazony"), new("krolikSmazony"), new("golabki"), new("pstragSmazony"), new("lososSmazony"), new("halibutSmazony"), new("okonSmazony"), new("sledzSmazony"),
         new("plackiZiemniaczane"), new("frytki"), new("pierogiRuskie"), new("pierogiZMiesem"), new("pierogiZKapustaIGrzybami"), new("pierogiZOwocami"), new("kopytka"), new("knedle"), new("nalesniki"), new("racuchy")];
-        public static List<Obiad> DaniaZGarnka = [new("karkowka")];
+        public static List<Obiad> DaniaZGarnka = [new("rosol"), new("barszczCzerwony"), new("zurek"), new("krupnik"), new("zupaPomidorowa"), new("zupaOgorkowa"), new("zupaGrzybowa"), new("kapusniak"), new("grochowka"),
+        new("zupaFasolowa"), new("zupaCebulowa"), new("chlodnik"), new("kremZBrokulow"), new("karkowka")];
         public static List<Obiad> StartoweObiady() => DaniaZPatelni.Concat(DaniaZGarnka).Select(d => new Obiad(d.Nazwa)).ToList();
     }
 }
