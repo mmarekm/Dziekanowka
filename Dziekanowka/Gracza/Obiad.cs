8c81ab23fdd9251e855f6c7908a3ea4c291375e8
marek <marekwpracymalicki@gmail.com>
2026-02-13 20:37:11 +0100

    bez knedli z patelni

--- Dziekanowka/Gracza/Obiad.cs
+++ Dziekanowka/Gracza/Obiad.cs
@@ -6,9 +6,9 @@
         public int Ilosc { get; set; } = 0;
         public static List<Obiad> DaniaZPatelni = [new("jajecznica"), new("jajkoSadzone"), new("omlet"), new("shakshuka"), new("kotletSchabowy"), new("kotletMielony"), new("stek"),
         new("piersZKurczaka"), new("bitki"), new("indykSmazony"), new("krolikSmazony"), new("golabki"), new("pstragSmazony"), new("lososSmazony"), new("halibutSmazony"), new("okonSmazony"), new("sledzSmazony"),
-        new("plackiZiemniaczane"), new("frytki"), new("pierogiRuskie"), new("pierogiZMiesem"), new("pierogiZKapustaIGrzybami"), new("pierogiZOwocami"), new("kopytka"), new("knedle"), new("nalesniki"), new("racuchy")];
+        new("plackiZiemniaczane"), new("frytki"), new("pierogiRuskie"), new("pierogiZMiesem"), new("pierogiZKapustaIGrzybami"), new("pierogiZOwocami"), new("kopytka"), new("nalesniki"), new("racuchy")];
         public static List<Obiad> DaniaZGarnka = [new("rosol"), new("barszczCzerwony"), new("zurek"), new("krupnik"), new("zupaPomidorowa"), new("zupaOgorkowa"), new("zupaGrzybowa"), new("kapusniak"), new("grochowka"),
-        new("zupaFasolowa"), new("zupaCebulowa"), new("chlodnik"), new("kremZBrokulow"), new("karkowka")];
+        new("zupaFasolowa"), new("zupaCebulowa"), new("chlodnik"), new("kremZBrokulow"), new("kremZDyni"), new("karkowka"), new("knedle")];
         public static List<Obiad> StartoweObiady() => DaniaZPatelni.Concat(DaniaZGarnka).Select(d => new Obiad(d.Nazwa)).ToList();
     }
 }
