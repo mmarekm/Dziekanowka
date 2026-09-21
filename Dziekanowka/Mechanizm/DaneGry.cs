596e1abe6cb878f57f37f1fe75edb9952d0fb22f
mmarekm <150899347+mmarekm@users.noreply.github.com>
2026-09-21 10:11:59 +0200

    Change HistoriaNotowanGieldy to HistoriaNotowanGieldyGlownej

--- Dziekanowka/Mechanizm/DaneGry.cs
+++ Dziekanowka/Mechanizm/DaneGry.cs
@@ -10,6 +10,6 @@ namespace Dziekanowka.Mechanizm
         public int Gielda { get; set; } = 100;
         public int DzienLogowania { get; set; } = 0;
         public int MiesiacLogowania { get; set; } = 0;
-        public List<int> HistoriaNotowanGieldy { get; set; } = [];
+        public List<NotowanieGieldy> HistoriaNotowanGieldyGlownej { get; set; } = [];
     }
 }
